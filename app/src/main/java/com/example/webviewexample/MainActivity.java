717a8b18cefe13382c33887539c4b54526ec3dd1
Goncharov01 <mr.sacha.goncharov.01@mail.ru>
2020-05-19 13:39:46 +0300

    AddUploadPhoto

--- app/src/main/java/com/example/webviewexample/MainActivity.java
+++ app/src/main/java/com/example/webviewexample/MainActivity.java
@@ -1,18 +1,32 @@
 package com.example.webviewexample;
 
+import androidx.annotation.RequiresApi;
 import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.ActivityNotFoundException;
+import android.content.Intent;
 import android.content.SharedPreferences;
 import android.content.res.Configuration;
+import android.net.Uri;
+import android.os.Build;
 import android.os.Bundle;
 import android.preference.PreferenceManager;
 import android.webkit.CookieManager;
+import android.webkit.ValueCallback;
+import android.webkit.WebChromeClient;
 import android.webkit.WebSettings;
 import android.webkit.WebView;
 import android.webkit.WebViewClient;
-
+import android.widget.Toast;
 import java.util.Locale;
 
+
 public class MainActivity extends AppCompatActivity {
+
+    private ValueCallback<Uri> mUploadMessage;
+    public ValueCallback<Uri[]> uploadMessage;
+    public static final int REQUEST_SELECT_FILE = 100;
+    private final static int FILECHOOSER_RESULTCODE = 1;
 
     WebView webView;
     String mLang;
@@ -29,13 +43,36 @@ public class MainActivity extends AppCompatActivity {
 
         SharedPreferences sp = PreferenceManager.getDefaultSharedPreferences(this);
 
+        webView.setWebChromeClient(new WebChromeClient() {
+            @RequiresApi(api = Build.VERSION_CODES.LOLLIPOP)
+            public boolean onShowFileChooser(WebView mWebView, ValueCallback<Uri[]> filePathCallback, WebChromeClient.FileChooserParams fileChooserParams) {
+                if (uploadMessage != null) {
+                    uploadMessage.onReceiveValue(null);
+                    uploadMessage = null;
+                }
+
+                uploadMessage = filePathCallback;
+
+                Intent intent = fileChooserParams.createIntent();
+                try {
+                    startActivityForResult(intent, REQUEST_SELECT_FILE);
+                } catch (ActivityNotFoundException e) {
+                    uploadMessage = null;
+                    Toast.makeText(MainActivity.this, "Cannot Open File Chooser", Toast.LENGTH_LONG).show();
+                    return false;
+                }
+                return true;
+            }
+
+        });
+
+
         if (savedInstanceState != null) {
             ((WebView) findViewById(R.id.wb)).restoreState(savedInstanceState.getBundle("webViewState"));
             webView.loadUrl(sp.getString("SAVED_URL", "https://navsegda.net"));
         } else {
 
             if (mLang.equals("ru")) {
-//                webView.loadUrl("https://m.navsegda.net/rest/v2/user/settings/locale");
                 webView.loadUrl(sp.getString("SAVED_URL", "https://navsegda.net"));
                 System.out.println("Russian!!!" + mLang);
             } else {
@@ -46,7 +83,6 @@ public class MainActivity extends AppCompatActivity {
         }
 
         settingWeb();
-
     }
 
     class HelloWebViewClient extends WebViewClient {
@@ -56,7 +92,6 @@ public class MainActivity extends AppCompatActivity {
             webview.loadUrl(url);
             return true;
         }
-
 
         @Override
         public void onLoadResource(WebView view, String url) {
@@ -122,5 +157,25 @@ public class MainActivity extends AppCompatActivity {
         }
     }
 
+    @Override
+    public void onActivityResult(int requestCode, int resultCode, Intent intent) {
+        super.onActivityResult(requestCode, resultCode, intent);
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.LOLLIPOP) {
+            if (requestCode == REQUEST_SELECT_FILE) {
+                if (uploadMessage == null)
+                    return;
+                uploadMessage.onReceiveValue(WebChromeClient.FileChooserParams.parseResult(resultCode, intent));
+                uploadMessage = null;
+            }
+        } else if (requestCode == FILECHOOSER_RESULTCODE) {
+            if (null == mUploadMessage)
+                return;
+            Uri result = intent == null || resultCode != MainActivity.RESULT_OK ? null : intent.getData();
+            mUploadMessage.onReceiveValue(result);
+            mUploadMessage = null;
+        } else
+            Toast.makeText(MainActivity.this.getApplicationContext(), "Failed to Upload Image", Toast.LENGTH_LONG).show();
+
+    }
 
 }
